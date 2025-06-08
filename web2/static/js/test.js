618abcacda980a3705ed3a3572f8a455bb1dd04a
maksiscool <140883386+maksiscool@users.noreply.github.com>
2025-06-08 15:05:12 +0300

    Add files via upload

--- web2/static/js/test.js
+++ web2/static/js/test.js
@@ -35,13 +35,23 @@ function updatePos(ev) {
 
 }
 document.onmousemove = updatePos
+var menu = document.getElementById("menu")
 var rocket = document.getElementById("tennis")
-var rocket_x = 500
-var rocket_y = 500
+var rocket_x = menu.getBoundingClientRect().left + menu.getBoundingClientRect().width/2
+var rocket_y = window.screen.height * 0.75
+
+var p1x = rocket_x
+var p1y = rocket_y + 40
+var p2x = rocket_x
+var p2y = rocket_y - 30
+
+
 var rocket_dx = 0
 var rocket_dy = 0
 var speed = 5
 a = Date.now();
+rocket.style.left = `${500}px`
+rocket.style.top = `${500}px`
 function Tick() {
     var dt = (Date.now()-a)/1000
     
@@ -50,15 +60,41 @@ function Tick() {
 
     rocket_dx += tmpdx * speed* dt
     rocket_dy += tmpdy * speed* dt
-    rocket_dx *= 0.97
-    rocket_dy *= 0.97
 
+    mgtd = Math.sqrt(rocket_dx*rocket_dx+rocket_dy*rocket_dy)
+    if (mgtd > 1000) {
+        rocket_dx = 1000*(rocket_dx/mgtd)
+        rocket_dy = 1000*(rocket_dy/mgtd)
+    }
+
+    if (rocket.getBoundingClientRect().right+rocket_dx*dt > menu.getBoundingClientRect().right+40 && rocket_dx > 0) {
+        rocket_dx = 0
+    }
+    if (rocket.getBoundingClientRect().left+rocket_dx*dt < menu.getBoundingClientRect().left-40 && rocket_dx < 0) {
+        rocket_dx = 0
+    }
+    if (rocket.getBoundingClientRect().bottom+rocket_dy*dt > menu.getBoundingClientRect().bottom+40 && rocket_dy > 0) {
+        rocket_dy = 0
+    }
+    if (rocket.getBoundingClientRect().top+rocket_dy*dt < menu.getBoundingClientRect().bottom/2-40 && rocket_dy < 0) {
+        rocket_dy = 0
+    }
+    p1x += rocket_dx*dt
+    p1y += rocket_dy*dt
+    mgtd2 = Math.sqrt((p1x-p2x)*(p1x-p2x) + (p1y-p2y)*(p1y-p2y))
+    p2x = 70*(p2x-p1x)/mgtd2 + p1x
+    p2y = 70*(p2y-p1y)/mgtd2 + p1y
     
+
+
+
+
     rocket_x+=rocket_dx*dt
     rocket_y+=rocket_dy*dt
-    rocket.style.transform = `rotate(${Math.atan2(rocket_dy,Math.abs(rocket_dx))}rad)`
-    rocket.style.left = `${rocket_x - rocket.clientWidth/4}px`
-    rocket.style.top = `${rocket_y-rocket.clientHeight*0.7}px`
+    rocket.style.transform = `rotate(${Math.atan2((p1y-p2y),(p1x-p2x))-Math.PI/2}rad)`
+    rocket.style.left = `${rocket_x - rocket.clientWidth/2}px`
+    rocket.style.top = `${rocket_y-rocket.clientHeight/2}px`
+
     a = Date.now()
     setTimeout(Tick,16)
 }
